2731d7078e1b3dcee04336a07be9755e3dd1ad97
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-18 21:54:48 -0300

    Add Redux store

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import dashboardReducer from './features/dashboard/dashboardSlice';
 import authReducer from './features/auth/authSlice';
+import dashboardReducer from './features/dashboard/dashboardSlice';
 
 export const store = configureStore({
   reducer: {
-    dashboard: dashboardReducer,
     auth: authReducer,
+    dashboard: dashboardReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
